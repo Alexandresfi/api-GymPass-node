eb2be0270be7e372f9ebaa7b5882a245f7be80ec
Alexandre Nascimento <alexandre.sfi@hotmail.com>
2023-10-11 16:54:31 -0300

    feat: create users

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-prisma.user.create({
-  data: {
-    email: 'alexandre@hotmail.com',
-    name: 'Alexandre Nascimento',
-  },
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
 })
